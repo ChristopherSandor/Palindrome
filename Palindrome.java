d8afba905f31bb96a052f0ca338a3730870ec4fb
Christopher Sandor <71267879+ChristopherSandor@users.noreply.github.com>
2024-01-13 17:57:01 -0500

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,15 @@
+public class Palindrome {
+    public static void main(String[] args) {
+       
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        int num3 = Integer.parseInt(args[2]);
+        int num4 = Integer.parseInt(args[3]);
+        int num5 = Integer.parseInt(args[4]);
+        int num6 = Integer.parseInt(args[5]);
+
+        boolean test = (num1 == num6 && num2 == num5 && num3 == num4);
+        
+        System.out.println(test);
+    }
+}
